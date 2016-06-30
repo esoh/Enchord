a5638621f036a2e0e37c776572039524758ac0b4
Sean Oh <seanoh@seans-mbp-3.attlocal.net>
2016-06-30 12:33:51 -0700

    Create fret number labels

--- app/src/main/java/sstudio/gnote/FretboardView.java
+++ app/src/main/java/sstudio/gnote/FretboardView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -16,49 +17,74 @@ public class FretboardView extends View {
 
     private int w;
     private int h;
-    private Paint paint;
+    private Paint fretboardPaint;
+    private TextPaint fretNumberPaint;
+    private float textOffset;
+    private int numFrets;
 
     public FretboardView(Context context, AttributeSet attr) {
         super(context, attr);
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         final int version = Build.VERSION.SDK_INT;
+
+        fretboardPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fretNumberPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
         if (version >= 23) {
-            paint.setColor(ContextCompat.getColor(context, R.color.colorFretboard));
+            fretboardPaint.setColor(ContextCompat.getColor(context, R.color.colorFretboard));
+            fretNumberPaint.setColor(ContextCompat.getColor(context, R.color.colorFretboard));
         } else {
-            paint.setColor(context.getResources().getColor(R.color.colorFretboard));
+            fretboardPaint.setColor(context.getResources().getColor(R.color.colorFretboard));
+            fretNumberPaint.setColor(context.getResources().getColor(R.color.colorFretboard));
         }
+
+        fretNumberPaint.setTextAlign(Paint.Align.CENTER);
+        fretNumberPaint.setTextSize(getResources().getDimensionPixelSize(R.dimen.fret_number_font_size));
+        textOffset = ((fretNumberPaint.descent() - fretNumberPaint.ascent()) / 2) - fretNumberPaint.descent();
+    }
+
+    public void setNumFrets(int numFrets){
+        this.numFrets = numFrets;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        // nothing to draw. numFrets not set.
+        if(numFrets == 0){
+            return;
+        }
         Log.d("dimen", "width:" + w);
         Log.d("dimen", "height:" + h);
         int stringWidth = w/120;
         int fretHeight = h/250;
-        paint.setStrokeWidth(stringWidth);
-        //canvas.drawLine(w/2, 0,  w/2, h, paint);
-        int d = 70; // convert this into a proportion of w
-        int padding = 80; // convert this into a proportion of h
-        canvas.drawLine(w/2 + d/2, padding, w/2 + d/2, h-padding, paint);
-        canvas.drawLine(w/2 - d/2, padding, w/2 - d/2, h-padding, paint);
-        canvas.drawLine(w/2 + 3*d/2, padding, w/2 + 3*d/2, h-padding, paint);
-        canvas.drawLine(w/2 - 3*d/2, padding, w/2 - 3*d/2, h-padding, paint);
-        canvas.drawLine(w/2 + 5*d/2, padding, w/2 + 5*d/2, h-padding, paint);
-        canvas.drawLine(w/2 - 5*d/2, padding, w/2 - 5*d/2, h-padding, paint);
+        fretboardPaint.setStrokeWidth(stringWidth);
+        double d = w/7.71428571429; // convert this into a proportion of w
+        double padding = h/23.1; // convert this into a proportion of h
+
+        // draw the guitar strings
+        for(double i = w/2-5*d/2; i <= w/2+5*d/2; i+=d){
+            canvas.drawLine((float)i, (float)padding, (float)i, (float)(h-padding), fretboardPaint);
+        }
 
         double scaleLength = 1.50084054172*(h-2*padding) + padding;
         double currentFret = padding;
+        double prevFret;
+
         //draw the nut
-        paint.setStrokeWidth(fretHeight*2);
-        canvas.drawLine(w/2-5*d/2, (float)currentFret, w/2+5*d/2, (float)currentFret, paint);
-        currentFret = (scaleLength - currentFret)/17.817f + currentFret;
-        paint.setStrokeWidth(fretHeight);
-        for(int i = 0; i < 19; i++){
-            canvas.drawLine(w/2-5*d/2, (float)currentFret, w/2+5*d/2, (float)currentFret, paint);
-            currentFret = (scaleLength - currentFret)/17.817f + currentFret;
+        fretboardPaint.setStrokeWidth(fretHeight*2);
+        canvas.drawLine((float) (w/2-5*d/2), (float)currentFret, (float)(w/2+5*d/2), (float)currentFret, fretboardPaint);
+        prevFret = currentFret;
+        currentFret = (scaleLength - prevFret)/17.817f + prevFret;
+        float fretNumberY = (float) (prevFret + currentFret)/2;
+
+        //draw the frets
+        fretboardPaint.setStrokeWidth(fretHeight);
+        for(int i = 0; i < numFrets; i++){
+            canvas.drawText((i+1)+"", (float)(w/2-3*d), fretNumberY + textOffset, fretNumberPaint);
+            canvas.drawLine((float)(w/2-5*d/2), (float)currentFret, (float)(w/2+5*d/2), (float)currentFret, fretboardPaint);
+            prevFret = currentFret;
+            currentFret = (scaleLength - prevFret)/17.817f + prevFret;
+            fretNumberY = (float) (prevFret + currentFret)/2;
         }
 
-        //canvas.drawLine(0, h/2, w, h/2, paint);
     }
 
     @Override
--- app/src/main/java/sstudio/gnote/MainActivity.java
+++ app/src/main/java/sstudio/gnote/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //for now, numFrets is set.
+    private final int numFrets = 19;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FretboardView myFretboard = (FretboardView) findViewById(R.id.fretboard);
+        if(myFretboard != null) {
+            myFretboard.setNumFrets(numFrets);
+        }
     }
 }
